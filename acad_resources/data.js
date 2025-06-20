0df7c105860c8ed3a66b8561443420a6c9746090
Ketan <ketan.agarwal2506@gmail.com>
2025-06-21 00:05:19 +0530

    initially

---
+++ acad_resources/data.js
@@ -0,0 +1,295 @@
+const data = {
+    "MTH111": {
+        "name": "Single Variable Calculus",
+        "code": "MTH111",
+        "resources": {
+          "PYQs": {
+            "2023": [
+              { "title": "Quiz Solution", "link": "https://drive.google.com/file/d/1QGGn1oVQw1Qn4ldyZyVs1O0nulMij2uq/view?usp=drive_link" },
+              { "title": "Endsem Solution", "link": "https://drive.google.com/file/d/1CshrO_H8-W6OP54i9BiR-3c0kY0ZyrFf/view?usp=drive_link" },
+              { "title": "Endsem Common Mistakes", "link": "https://drive.google.com/file/d/1VZdQ_hHJtG4iQi0ACRW76WtxZ4Z0ZdJG/view?usp=drive_link" }
+            ],
+            "2022": [
+              { "title": "Quiz Solution", "link": "https://drive.google.com/file/d/1iqMzBsxP8yZoECNYBzM5LTIkuBbbATRK/view?usp=drive_link" },
+              { "title": "Quiz Marking Scheme", "link": "https://drive.google.com/file/d/14LrTP3oM7JteeO5XbwdOu1qaOkW2m682/view?usp=drive_link" },
+              { "title": "Endsem Solution", "link": "https://drive.google.com/file/d/1x1CRtKz6yfEV6P_YOGNw-zFULNZ5hvz2/view?usp=drive_link" }
+            ],
+            "2021": [
+              { "title": "Quiz Solution", "link": "https://drive.google.com/file/d/1-LhNzoUCeltZ5pb4LvcBRVRPocAwjuBi/view?usp=drive_link" },
+              { "title": "Midsem Solution", "link": "https://drive.google.com/file/d/1lXiG0h3Q3FLDQ2m4wqPIrY3IqR8aZst-/view?usp=drive_link" }
+            ],
+            "2020": [
+              { "title": "Quiz", "link": "https://drive.google.com/file/d/1Pahs8pa2fMMyNUh0A2Y-Hu70SVkv3qFS/view?usp=drive_link" },
+              { "title": "Quiz Solution", "link": "https://drive.google.com/file/d/1z1D6iUCEyduJd8eBj78carjbTzDqu0B4/view?usp=drive_link" }
+            ],
+            "2017": [
+                { "title": "Quiz 1", "link": "https://drive.google.com/file/d/1SId6syxl8UvB7r3iACSlSxG2zDq4wz_0/view?usp=drive_link"},
+                { "title": "Quiz 2", "link": "https://drive.google.com/file/d/1gVpFn7zwsG0Et-sAiDMaEyK6Y5sjkds5/view?usp=drive_link"},
+                { "title": "Midsem", "link": "https://drive.google.com/file/d/1vtLSSXoS_RbqEtUqsTTqVDwsrsyxn2bf/view?usp=drive_link"},
+                { "title": "Endsem", "link": "https://drive.google.com/file/d/1oGc4kVBa946ehMUfx2KpQAgBAKTi79S8/view?usp=drive_link"}
+            ],
+            "2016": [
+              { "title": "Quiz MS", "link": "https://drive.google.com/file/d/1IhW7C56FDMIgKcBk6smUJipVB4gni4sT/view?usp=drive_link" }
+            ],
+            "2015": [
+                { "title": "Quiz Solution", "link": "https://drive.google.com/file/d/1VUC_0F01-Y9cDOQ9bj7tiyY8bcdZvqLy/view?usp=drive_link" }
+              ],
+              "2014": [
+                { "title": "Quiz Solution", "link": "https://drive.google.com/file/d/1jg84lZV8NSvB4u5Ymps5pkpk5H4Tbb0a/view?usp=drive_link" }
+              ],
+              "2013": [
+                { "title": "Quiz 1 MS", "link": "https://drive.google.com/file/d/1sqoVCInhokR_Q0xNBSQaVE5fJh0ssfu3/view?usp=drive_link" },
+                { "title": "Quiz 2 MS", "link": "https://drive.google.com/file/d/1EEvBXzgV1StsavmTYANmGy8N9vinVHHP/view?usp=drive_link" }
+              ],
+              "2012": [
+                { "title": "Quiz Solution", "link": "https://drive.google.com/file/d/12l-BiZs8vVuScOfHf-ACB-c7dcUXaovN/view?usp=drive_link" },
+                { "title": "Midsem Solution", "link": "https://drive.google.com/file/d/1GQiVv3qmykF0ct3fU2YDhplnTkVbZ0Wq/view?usp=drive_link" }
+              ],
+              
+              "2011": [
+                { "title": "Quiz Solution", "link": "https://drive.google.com/file/d/1Uh2zE2pKmQGKQdqYAxjM-N9u8q9kdI_-/view?usp=drive_link" },
+                { "title": "Midsem Solution", "link": "https://drive.google.com/file/d/1eeBDYpY8-cFP4qc7c96c7Tu8POJSfFPG/view?usp=drive_link" }
+              ],
+              "2010": [
+                { "title": "Midsem 1 Solution", "link": "https://drive.google.com/file/d/19v2_UPc_c8U8tS8aMUj20YP7B5-QyhcH/view?usp=drive_link" },
+                { "title": "Midsem 2 Solution", "link": "https://drive.google.com/file/d/1E_iyKhzGk9nLxDNxt56lO7nwQ-qQBIXq/view?usp=drive_link" }
+              ],
+              "2009": [
+                { "title": "Midsem 1 Solution", "link": "https://drive.google.com/file/d/1ksglxGEiqg2z3HAZtRptXrsV-shkIFmx/view?usp=drive_link" },
+                { "title": "Midsem 2 Solution", "link": "https://drive.google.com/file/d/1D-ofvCXTitJWUGS1wTG55baqlvJ8LF2y/view?usp=drive_link" }
+              ]
+              ,
+              "2008": [
+                { "title": "Midsem 1 Solution", "link": "https://drive.google.com/file/d/10Q2JfMQAeriJWTjkexSVGnRXra1vaQyT/view?usp=drive_link" },
+                { "title": "Midsem 2 Solution", "link": "https://drive.google.com/file/d/1ChgBFNIGNO4TpIw5nNuiDRQYaeda7GPQ/view?usp=drive_link" }
+              ]
+          },
+          "Assignments": [
+            { "title": "Practice Problems 1", "link": "https://drive.google.com/file/d/1UiWoVGqwdzKvbDbjAiRKEWXmPXm7UNcv/view?usp=drive_link" },
+            { "title": "Practice Problems 2", "link": "https://drive.google.com/file/d/15Cbqq3EIldBwO9ohKOZSsSzCFDGHNy1s/view?usp=drive_link" },
+            { "title": "Practice Problems 3", "link": "https://drive.google.com/file/d/162vQ7wVZ0ig_0FkMg-21_FhUAfRXu3k_/view?usp=drive_link" },
+            { "title": "Practice Problems 4", "link": "https://drive.google.com/file/d/1PoXuAB8c7g-fyQwk-fq6SU-sZtZjCtv9/view?usp=drive_link" },
+            { "title": "Practice Problems 5", "link": "https://drive.google.com/file/d/1Cq1JAlIIAuH5RpsXuAqKzM-_rO47E2JL/view?usp=drive_link" },
+            { "title": "Practice Problems 6", "link": "https://drive.google.com/file/d/12aOlXKkRrC4FVHKRwy8XqGr3I6ttkwsm/view?usp=drive_link" },
+            { "title": "Practice Problems 7", "link": "https://drive.google.com/file/d/1KyZg_3GEcyuexndkF5HlEFL1Hn_KxaQB/view?usp=drive_link" },
+            { "title": "Practice Problems 8", "link": "https://drive.google.com/file/d/1z7TMzNIeEO7wMQ-wTfmthECWoj1no_YO/view?usp=drive_link" },
+            { "title": "Practice Problems 9", "link": "https://drive.google.com/file/d/1A7lCuh2P-38YFiUnkqd_cGJQ2ywrEt8z/view?usp=drive_link" },
+            { "title": "Practice Problems 10", "link": "https://drive.google.com/file/d/1FI-68hllj6NBq4vbTN8ueNPa4_otdXgp/view?usp=drive_link" },
+            { "title": "Practice Problems 12", "link": "https://drive.google.com/file/d/1oXF6HORsUqwtNJ38owHsq4ZbHInFz1Km/view?usp=drive_link" },
+            { "title": "Practice Problems 13", "link": "https://drive.google.com/file/d/1gBcZNDjI9WFbk11A1kZn7ObwFPShD0EY/view?usp=drive_link" },
+            { "title": "Practice Problems 14", "link": "https://drive.google.com/file/d/1DL52cTI_dpHdmSpjRa4qrf6CpF529PlV/view?usp=drive_link" },
+            { "title": "Practice Problems 15", "link": "https://drive.google.com/file/d/1QJ3_7SOuo5ETbWL2vy9bz0AJZ3ZByF4Q/view?usp=drive_link" },
+            { "title": "Practice Problems 16", "link": "https://drive.google.com/file/d/1IWKO_fcjpCajBZQxhvAOi4XkPIjGEDlt/view?usp=drive_link" },
+            { "title": "Practice Problems 17", "link": "https://drive.google.com/file/d/1NsLqGtmAXQGYNUaI_8OD_al9kbcIh5Jm/view?usp=drive_link" },
+            { "title": "Practice Problems 18", "link": "https://drive.google.com/file/d/12zb3FJ6TeTZzHqzUzUfl-mYxcM52Wl6R/view?usp=drive_link" },
+            { "title": "Practice Problems 18-2", "link": "https://drive.google.com/file/d/1nVu-HybH3Bz8h0oYB1JtjkDrmwSSA9xh/view?usp=drive_link" },
+            { "title": "Practice Problems 19", "link": "https://drive.google.com/file/d/13SgS0DNYx631ndiPd09hwQrcWzE_P3Oz/view?usp=drive_link" }
+          ],
+          "Class Notes": [
+          ],
+          "AM Notes": [
+            { "title": "AM Notes - I and II 2024-25", "link": "https://drive.google.com/file/d/12XlCDkeBTYkabCa_pZdqzLNZ_sFnAP7L/view?usp=drive_link" },
+            { "title": "AM Notes - III 2024-25", "link": "https://drive.google.com/file/d/18WlXgHfFm7vutEZeEBffSKtjBVo2LMqS/view?usp=drive_link" },
+            { "title": "AM Notes Slides-1", "link": "https://docs.google.com/presentation/d/1rb84pg1DPb0kA0rf-JxJWIjLmVIjXHlS/edit?usp=drive_link&ouid=118171121564066809461&rtpof=true&sd=true" },
+            { "title": "AM Notes Slides-2", "link": "https://docs.google.com/presentation/d/1IrXxvBXOB3TboZUVUKLFxvLwD1eUR8QN/edit?usp=drive_link&ouid=118171121564066809461&rtpof=true&sd=true" }
+
+            
+          ],
+          "External Links": [
+            {"title": "2024-25 Course WebPage (Amit Kuber Sir)", "link": "https://sites.google.com/view/exploring-infinity-within/teaching/mth111m-24-25"}, 
+            {"title": "2020-21 Course WebPage (PS Raj Sir)", "link": "https://home.iitk.ac.in/~psraj/mth101/index.html"}
+              
+          ]
+        }
+      },
+    "MTH112": {
+      "name": "Application of Single Variable Calculus",
+      "code": "MTH112",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "MTH113": {
+      "name": "Linear Algebra",
+      "code": "MTH113",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "MTH114": {
+      "name": "Ordinary Differential Equations",
+      "code": "MTH114",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "PHY111": {
+      "name": "Physics Laboratory",
+      "code": "PHY111",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "PHY112": {
+      "name": "Classical Dynamics",
+      "code": "PHY112",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "PHY113": {
+      "name": "Introduction to Electromagnetism",
+      "code": "PHY113",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "PHY114": {
+      "name": "Introduction to Quantum Mechanics",
+      "code": "PHY114",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "PHY115": {
+      "name": "Oscillation and Waves",
+      "code": "PHY115",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "CHM111": {
+      "name": "Chemistry Laboratory",
+      "code": "CHM111",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "CHM112": {
+      "name": "General Chemistry",
+      "code": "CHM112",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "CHM113": {
+      "name": "General Chemistry",
+      "code": "CHM113",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "TA111": {
+      "name": "Technical Arts",
+      "code": "TA111",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "LIF111": {
+      "name": "Introduction to Biology",
+      "code": "LIF111",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "ELC111": {
+      "name": "English Language and Communication",
+      "code": "ELC111",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "ELC112": {
+      "name": "English Language and Communication",
+      "code": "ELC112",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "ELC113": {
+      "name": "English Language and Communication",
+      "code": "ELC113",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "ESC111": {
+      "name": "Fundamental of Computing - I",
+      "code": "ESC111",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "ESC112": {
+      "name": "Fundamental of Computing - II",
+      "code": "ESC112",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    },
+    "ESC113": {
+      "name": "Fundamental of Computing - III",
+      "code": "ESC113",
+      "resources": {
+        "PYQs": [],
+        "Assignments": [],
+        "ClassNotes": [],
+        "AMNotes": []
+      }
+    }
+  }
